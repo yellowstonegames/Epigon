7135093e5de56ec026d562cba3011cfbb9861792
Eben Howard <SquidPony@users.noreply.github.com>
2017-06-27 22:08:19 -0700

    rework MovementAction to not use an Element

--- Proto/core/src/squidpony/epigon/actions/MovementAction.java
+++ Proto/core/src/squidpony/epigon/actions/MovementAction.java
@@ -1,13 +1,14 @@
 package squidpony.epigon.actions;
 
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import squidpony.squidgrid.Direction;
 import squidpony.squidmath.Coord;
 
 import squidpony.epigon.data.specific.Physical;
-import squidpony.epigon.universe.Element;
 
 /**
  * Represents an action attempting to move the target.
@@ -18,7 +19,6 @@ public class MovementAction implements Action {
 
     public Coord start;
     public Physical mover;
-    public Element element;
     public boolean forced;
     public Queue<Coord> moveList;//movements in order of execution
 
@@ -30,60 +30,31 @@ public class MovementAction implements Action {
      * @param end
      */
     public MovementAction(Coord start, Physical mover, Coord end) {
-        moveList = new LinkedList<>();
-        moveList.add(end);
-        initialize(start, mover, null, false, moveList);
+        this(start, mover, Collections.singletonList(end), false);
     }
 
     /**
      * Constructor which sets the end point as provided.
-     *
-     * @param start
-     * @param mover
-     * @param element
-     * @param forced
-     * @param end
      */
-    public MovementAction(Coord start, Physical mover, Element element, boolean forced, Coord end) {
-        moveList = new LinkedList<>();
-        moveList.add(end);
-        initialize(start, mover, element, forced, moveList);
+    public MovementAction(Coord start, Physical mover, Coord end, boolean forced) {
+        this(start, mover, Collections.singletonList(end), forced);
     }
 
     /**
      * Constructor which sets the point in the given direction as the end point.
-     *
-     * @param start
-     * @param mover
-     * element key
-     * @param forced
-     * @param dir
      */
-    public MovementAction(Coord start, Physical mover, Element element, boolean forced, Direction dir) {
-        moveList = new LinkedList<>();
-        moveList.add(Coord.get(start.x + dir.deltaX, start.y + dir.deltaY));
-        initialize(start, mover, element, forced, moveList);
+    public MovementAction(Coord start, Physical mover, Direction dir, boolean forced) {
+        this(start, mover, Collections.singletonList(Coord.get(start.x + dir.deltaX, start.y + dir.deltaY)), forced);
     }
 
     /**
      * Constructor which takes a complete queue of points for the movement path. The starting
      * location should not be in the queue, but the end location must be.
-     *
-     * @param start
-     * @param mover
-     * @param element
-     * @param forced
-     * @param moveList
      */
-    public MovementAction(Coord start, Physical mover, Element element, boolean forced, Queue<Coord> moveList) {
-        initialize(start, mover, element, forced, moveList);
-    }
-
-    private void initialize(Coord start, Physical mover, Element element, boolean forced, Queue<Coord> moveList) {
+    public MovementAction(Coord start, Physical mover, List<Coord> moveList, boolean forced) {
         this.start = start;
         this.mover = mover;
-        this.element = element;
         this.forced = forced;
-        this.moveList = moveList;
+        this.moveList = new LinkedList<>(moveList);
     }
 }
