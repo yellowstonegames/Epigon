445968b478bf9671997def1913e7a99a3a37d786
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-07-02 22:23:39 -0700

    Bring back stat/info panel. Adjust Stat.toString()
    Stat would previously capitalize the already-upper-case first letter and
    leave the rest still in upper case. Element has some commentary.

--- Proto/core/src/squidpony/epigon/universe/Element.java
+++ Proto/core/src/squidpony/epigon/universe/Element.java
@@ -18,23 +18,31 @@ public enum Element { // TODO - make this a full EpiData class with external dat
     FIRE(NATURE, "fire", "hot hot hot", SColor.FIREBRICK),
     MAGMA(FIRE, "magma", "flowing hotness", SColor.BURNT_ORANGE),
     AIR(NATURE),
+    // if you have magma, then smoke makes sense.
     SONIC(NATURE),
     ACID(NATURE),
-    ALKALAI(NATURE),
+    ALKALAI(NATURE), // Misspelled. A correct alternative would be ALKALI, ALKALINE, or possibly CAUSTIC
     ICE(NATURE),
     WATER(NATURE),
+    // You probably want electrical/lightning. This is a bizarre omission as it stands.
+    // Maybe you want a disease element? Maybe radiation (possibly magical or nuclear)?
     // Magical
     MAGIC(),
-    GEOMANCY(MAGIC),
+    GEOMANCY(MAGIC), // Is this ley line location magic, feng-shui-like sacred geometry, or just dirt magic?
     CHRONOMANCY(MAGIC),
+    // transforming (polymorph) attacks work well as an element that, instead of killing, changes the target's shape
+    // may want divine/morally-based magic, which might just smite anyone who isn't the user
+    // may want magic based on authority/bond-of-word/pacts, to fit the theme of a relative of royalty
+    // may want attacks via teleporting the target to be an element
     // Astronomical
     ASTRONOMICAL(),
     SOLAR(ASTRONOMICAL),
     LUNAR(ASTRONOMICAL),
     STELLAR(ASTRONOMICAL),
-    COMETARY(ASTRONOMICAL),
+    COMETARY(ASTRONOMICAL), // Why both cometary and meteoric but no black hole magic? SINGULAR, I'd say.
     PLANETARY(ASTRONOMICAL),
     METEORIC(ASTRONOMICAL),
+    // maybe you want a VACUOUS element for the empty void between cosmic bodies?
     // Misc
     LIGHT(null, "light", "Basic lighting, not related to magical or other effects.", SColor.ALICE_BLUE),
     DEATH(),
