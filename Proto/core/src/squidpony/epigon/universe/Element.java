aeb91f86b699504f5d728736668b4138f3469a06
Eben Howard <SquidPony@users.noreply.github.com>
2017-08-12 23:20:40 -0700

    Colors for all the Elements

--- Proto/core/src/squidpony/epigon/universe/Element.java
+++ Proto/core/src/squidpony/epigon/universe/Element.java
@@ -7,17 +7,17 @@ import squidpony.squidgrid.gui.gdx.SColor;
  */
 public enum Element { // TODO - make this a full EpiData class with external data source
     // Mundane
-    MUNDANE(),
+    MUNDANE(SColor.MOUNTBATTEN_PINK),
     PIERCE(MUNDANE),
     LACERATE(MUNDANE),
     BLUNT(MUNDANE),
     // Natural
-    NATURE(),
-    STONE(NATURE),
-    CRYSTAL(STONE),
+    NATURE(SColor.FOREST_GREEN),
+    STONE(NATURE, SColor.SLATE_GRAY),
+    CRYSTAL(STONE, SColor.CREAM),
     FIRE(NATURE, "fire", "hot hot hot", SColor.ORANGE_PEEL),
     MAGMA(FIRE, "magma", "flowing hotness", SColor.CW_BRIGHT_ORANGE),
-    AIR(NATURE),
+    AIR(NATURE, SColor.BRIGHT_TURQUOISE),
     // if you have magma, then smoke makes sense.
     SONIC(NATURE, "sonic", "THEN THE BASS GOES BRRRRR WRRRR WOOP WOOP WOOP", new SColor(0xE3, 0xD7, 0xB4, 0x55, "Smoky Glass")),
     ACID(NATURE, "acid", "*Kssshh...* AYEEEE, IT BURNS!", SColor.CW_BRIGHT_LIME),
@@ -25,34 +25,34 @@ public enum Element { // TODO - make this a full EpiData class with external dat
     ICE(NATURE, "ice", "frosty chillin", SColor.CW_PALE_AZURE),
     WATER(NATURE, "water", "go with the flow", SColor.CW_FLUSH_AZURE),
     LIGHTNING(NATURE, "lightning", "DON'T TASE ME BRO", SColor.CW_LIGHT_VIOLET),
-    POISON(NATURE),
-    DISEASE(NATURE),
-    RADIATION(NATURE),
+    POISON(NATURE, SColor.PEAR),
+    DISEASE(NATURE, SColor.MOUSY_WISTERIA),
+    RADIATION(NATURE, SColor.LIME),
 
     // Magical
-    MAGIC(),
-    GEOMANCY(MAGIC), // Is this ley line location magic, feng-shui-like sacred geometry, or just dirt magic? Answer: just dirt
-    CHRONOMANCY(MAGIC),
+    MAGIC(SColor.CW_FLUSH_AZURE),
+    GEOMANCY(MAGIC, SColor.DISTANT_RIVER_BROWN), // Is this ley line location magic, feng-shui-like sacred geometry, or just dirt magic? Answer: just dirt
+    CHRONOMANCY(MAGIC, SColor.DB_EGGSHELL),
     // transforming (polymorph) attacks work well as an element that, instead of killing, changes the target's shape
     // may want divine/morally-based magic, which might just smite anyone who isn't the user
     // may want magic based on authority/bond-of-word/pacts, to fit the theme of a relative of royalty
     // may want attacks via teleporting the target to be an element
     // Astronomical
-    ASTRONOMICAL(),
-    SOLAR(ASTRONOMICAL),
-    LUNAR(ASTRONOMICAL),
-    STELLAR(ASTRONOMICAL),
-    COMETARY(ASTRONOMICAL), // Why both cometary and meteoric but no black hole magic? SINGULAR, I'd say.
-    GRAVITY(ASTRONOMICAL),
-    PLANETARY(ASTRONOMICAL),
-    METEORIC(ASTRONOMICAL),
-    VACUOUS(ASTRONOMICAL),
+    ASTRONOMICAL(SColor.CW_PALE_YELLOW),
+    SOLAR(ASTRONOMICAL, SColor.CW_LIGHT_GOLD),
+    LUNAR(ASTRONOMICAL, SColor.SILVER),
+    STELLAR(ASTRONOMICAL, SColor.BLUE_GREEN),
+    COMETARY(ASTRONOMICAL, SColor.DB_PLATINUM), // Why both cometary and meteoric but no black hole magic? SINGULAR, I'd say.
+    GRAVITY(ASTRONOMICAL, SColor.DB_STORM_CLOUD),
+    PLANETARY(ASTRONOMICAL, SColor.CW_FADED_APRICOT),
+    METEORIC(ASTRONOMICAL, SColor.BRIGHT_GOLDEN_YELLOW),
+    VACUOUS(ASTRONOMICAL, SColor.DB_CERULEAN),
     // Misc
     LIGHT(null, "light", "Basic lighting, not related to magical or other effects.", SColor.ALICE_BLUE),
-    DEATH(),
-    LIFE(),
-    LANGUAGE(),
-    COLOR();
+    DEATH(SColor.SISKIN_SPROUT_YELLOW),
+    LIFE(SColor.CW_BRIGHT_RED),
+    LANGUAGE(SColor.FLORAL_LEAF),
+    COLOR(SColor.INDIGO_WHITE);
 
     public Element parent;
     public String name;
@@ -60,17 +60,24 @@ public enum Element { // TODO - make this a full EpiData class with external dat
     public SColor color;
 
     private Element() {
-        this(null);
+        this(SColor.WHITE);
+    }
+
+    private Element(SColor color) {
+        this(null, "", "", color);
     }
 
     private Element(Element parent) {
-        this(parent, "", "", SColor.BLACK);
-        name = name().toLowerCase();
+        this(parent, "", "", parent == null ? SColor.WHITE : parent.color);
+    }
+
+    private Element(Element parent, SColor color) {
+        this(parent, "", "", color);
     }
 
     private Element(Element parent, String name, String description, SColor color) {
         this.parent = parent;
-        this.name = name;
+        this.name = name == null || name.isEmpty() ? name().toLowerCase() : name;
         this.description = description;
         this.color = color;
     }
