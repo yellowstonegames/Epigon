6f8db649a2c430ad98498ae2e8f42158bcc0c1d5
Eben Howard <SquidPony@users.noreply.github.com>
2017-09-14 07:35:17 -0700

    continued keybinding stubbing

--- Proto/core/src/squidpony/epigon/input/ControlMapping.java
+++ Proto/core/src/squidpony/epigon/input/ControlMapping.java
@@ -1,5 +1,6 @@
 package squidpony.epigon.input;
 
+import squidpony.squidgrid.gui.gdx.SquidInput;
 import squidpony.squidmath.OrderedMap;
 
 /**
@@ -7,10 +8,29 @@ import squidpony.squidmath.OrderedMap;
  */
 public class ControlMapping {
 
-    public static final OrderedMap<Verb, Character> defaultMapping;
+    public static final OrderedMap<Character, Verb> defaultMapping;
 
     static {
         defaultMapping = new OrderedMap<>();
+        defaultMapping.put('a', Verb.ANOINT);
+        defaultMapping.put('c', Verb.CLOSE);
+        defaultMapping.put('C', Verb.CONSUME);
+        defaultMapping.put('x', Verb.EXAMINE);
+        defaultMapping.put('f', Verb.FIRE);
+        defaultMapping.put('g', Verb.GET);
+        defaultMapping.put('G', Verb.GET_AOE);
+        defaultMapping.put('h', Verb.HELP);
+        defaultMapping.put('?', Verb.HELP);
+        defaultMapping.put(SquidInput.F1, Verb.HELP);
+        defaultMapping.put('i', Verb.INTERACT);
+        defaultMapping.put('I', Verb.INVENTORY);
+        defaultMapping.put('l', Verb.LOOK);
+        defaultMapping.put(SquidInput.DOWN_ARROW, Verb.MOVE_DOWN);
+        defaultMapping.put(SquidInput.DOWN_LEFT_ARROW, Verb.MOVE_DOWN_LEFT);
+        defaultMapping.put(SquidInput.DOWN_RIGHT_ARROW, Verb.MOVE_DOWN_RIGHT);
+        defaultMapping.put('>', Verb.MOVE_HIGHER);
+        defaultMapping.put('w', Verb.WEAR);
+        defaultMapping.put('W', Verb.WIELD);
     }
 
 }
--- Proto/core/src/squidpony/epigon/input/Verb.java
+++ Proto/core/src/squidpony/epigon/input/Verb.java
@@ -6,32 +6,35 @@ package squidpony.epigon.input;
  * Tied to user input options.
  */
 public enum Verb {
-    WEAR, // includes armor, clothing, and jewelry
-    WIELD, // includes weaponse, magic items that must be held to work, shields
     ANOINT, // for powders, gels, liquids, or anything that can be smeared onto something (the actor is doing this to themselves, if targetting other then use throw verb action)
+    CLOSE, // for doors, Interact should be for container manipulation
     CONSUME, // eat, drink, snort
     EXAMINE, // all five senses used to pay particular attention to an area
-    USE_SKILL,
-    USE_ABILITY,
-    MOVE_UP,
-    MOVE_UP_RIGHT,
-    MOVE_RIGHT,
-    MOVE_DOWN_RIGHT,
-    MOVE_DOWN,
-    MOVE_DOWN_LEFT,
-    MOVE_LEFT,
-    MOVE_UP_LEFT,
-    // Moving vertically should be based on interacting with a stairs object
-    REST,
-    THROW,
     FIRE, // Using an equipped weapon with ammo
     GET, // Get from tile currently occupied
     GET_AOE, // Get from all near tiles
+    HELP, // manual, key bindings, hints n tips
+    INTERACT, // Generic interaction with world environment, catch-all for anything not covered by specific verb basically
     INVENTORY,
-    // TODO - submenu interactions
-
-    SAVE,
-    QUIT,
     LOOK,
-    HELP // manual, key bindings, hints n tips
+    MOVE_DOWN,
+    MOVE_DOWN_LEFT,
+    MOVE_DOWN_RIGHT,
+    MOVE_HIGHER, // Up in 3D space, but "up" is "north" so "higher".... for North to always be actual North would be weird at the poles
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    MOVE_UP,
+    MOVE_UP_LEFT,
+    MOVE_UP_RIGHT,
+    MOVE_LOWER, // Down in 3D space
+    OPEN, // For doors only, interact should be used for containers
+    QUIT,
+    REST,
+    SAVE,
+    THROW,
+    USE_ABILITY,
+    WEAR, // includes armor, clothing, and jewelry
+    WIELD // includes weaponse, magic items that must be held to work, shields
+
+    // TODO - submenu interactions
 }
