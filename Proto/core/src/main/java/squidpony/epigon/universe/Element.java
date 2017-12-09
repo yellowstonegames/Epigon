b0f04729c66f106bb46b7cf0d3ee6c67994aae3b
Tommy Ettinger <tommy.ettinger@gmail.com>
2017-12-08 22:50:37 -0800

    Elements can be associated with attacks now
    Their color is shown with their name (where the name is a descriptor for
    the damage type), and one element is selected at random from the weapon
    per attack. One element is always more likely than the other if there
    are two.

--- Proto/core/src/main/java/squidpony/epigon/universe/Element.java
+++ Proto/core/src/main/java/squidpony/epigon/universe/Element.java
@@ -1,6 +1,7 @@
 package squidpony.epigon.universe;
 
 import com.badlogic.gdx.graphics.Color;
+import squidpony.StringKit;
 import squidpony.squidgrid.gui.gdx.SColor;
 
 /**
@@ -9,46 +10,51 @@ import squidpony.squidgrid.gui.gdx.SColor;
 public enum Element { // TODO - make this a full EpiData class with external data source
     // Mundane
     MUNDANE(SColor.MOUNTBATTEN_PINK),
-    PIERCE(MUNDANE),
-    LACERATE(MUNDANE),
+    PIERCING(MUNDANE),
+    SLASHING(MUNDANE),
     BLUNT(MUNDANE),
     // Natural
     NATURE(SColor.FOREST_GREEN),
-    STONE(NATURE, SColor.SLATE_GRAY),
-    CRYSTAL(STONE, SColor.CREAM),
+    EARTH(NATURE, SColor.SLATE_GRAY),
+    CRYSTAL(EARTH, new Color(0xE0FFFF99)),
     FIRE(NATURE, "fire", "hot hot hot", SColor.ORANGE_PEEL),
     MAGMA(FIRE, "magma", "flowing hotness", SColor.CW_BRIGHT_ORANGE),
     AIR(NATURE, SColor.BRIGHT_TURQUOISE),
     // if you have magma, then smoke makes sense.
-    SONIC(NATURE, "sonic", "THEN THE BASS GOES BRRRRR WRRRR WOOP WOOP WOOP", new SColor(0xE3, 0xD7, 0xB4, 0x55, "Smoky Glass")),
-    ACID(NATURE, "acid", "*Kssshh...* AYEEEE, IT BURNS!", SColor.CW_BRIGHT_LIME),
-    CAUSTIC(NATURE, "caustic", "lay in the lye, just may die", SColor.CW_SEAFOAM),
-    ICE(NATURE, "ice", "frosty chillin", SColor.CW_PALE_AZURE),
+    SOUND(NATURE, "sonic", "THEN THE BASS GOES BRRRRR WRRRR WOOP WOOP WOOP", new Color(0xE3D7B455)),
     WATER(NATURE, "water", "go with the flow", SColor.CW_FLUSH_AZURE),
+    ICE(WATER, "ice", "frosty chillin", SColor.CW_PALE_AZURE),
     LIGHTNING(NATURE, "lightning", "DON'T TASE ME BRO", SColor.CW_LIGHT_VIOLET),
     POISON(NATURE, SColor.PEAR),
-    DISEASE(NATURE, SColor.MOUSY_WISTERIA),
-    RADIATION(NATURE, SColor.LIME),
-
+    ACID(POISON, "acid", "*Kssshh...* AYEEEE, IT BURNS!", SColor.CW_BRIGHT_LIME),
+    CAUSTIC(POISON, "caustic", "lay in the lye, just may die", SColor.CW_SEAFOAM),
+    DISEASE(POISON, SColor.MOUSY_WISTERIA),
+    RADIATION(POISON, SColor.LIME),
     // Magical
-    MAGIC(SColor.CW_FLUSH_AZURE),
+    MAGIC(SColor.CW_FLUSH_AZURE), // Do Magic!
+    SHINING(MAGIC, SColor.WHITE), // light that burns! especially dungeon-dwelling monsters!
+    SHADOW(MAGIC, new Color(0x111111BB)), // darkness doesn't really do damage normally, so this is magic
     GEOMANCY(MAGIC, SColor.DISTANT_RIVER_BROWN), // Is this ley line location magic, feng-shui-like sacred geometry, or just dirt magic? Answer: just dirt
-    CHRONOMANCY(MAGIC, SColor.DB_EGGSHELL),
+    CHRONOMANCY(MAGIC, SColor.DB_EGGSHELL), // wibbly-wobbly timey-wimey bits
+    FATEFUL(MAGIC, SColor.VEGAS_GOLD), // luck, fortune and destined damage
+    PURE(MAGIC, new Color(0xCCEEFF88)), // negatively affects horrible things (undead, demons), does some damage to despoilers of this world
     // transforming (polymorph) attacks work well as an element that, instead of killing, changes the target's shape
-    // may want divine/morally-based magic, which might just smite anyone who isn't the user
+    CHANNELING(MAGIC, SColor.INTERNATIONAL_KLEIN_BLUE),
     // may want magic based on authority/bond-of-word/pacts, to fit the theme of a relative of royalty
-    CONTRACTUAL(MAGIC, SColor.STEEL_BLUE),
-    // may want attacks via teleporting the target to be an element
+    CONTRACTUAL(CHANNELING, SColor.STEEL_BLUE), // pact magic or magic that damages based on some powerful authority
+    SINISTER(CHANNELING, SColor.BURGUNDY), // stealing power from gods and/or demons and diverting the punishment to others
+    DIVINE(CHANNELING, SColor.LEMON_CHIFFON), // faith-based power granted by gods to their followers
     // Astronomical
-    ASTRONOMICAL(SColor.CW_PALE_YELLOW),
-    SOLAR(ASTRONOMICAL, SColor.CW_LIGHT_GOLD),
-    LUNAR(ASTRONOMICAL, SColor.SILVER),
-    STELLAR(ASTRONOMICAL, SColor.BLUE_GREEN),
-    COMETARY(ASTRONOMICAL, SColor.DB_PLATINUM), // Why both cometary and meteoric but no black hole magic? SINGULAR, I'd say.
-    GRAVITY(ASTRONOMICAL, SColor.DB_STORM_CLOUD),
-    PLANETARY(ASTRONOMICAL, SColor.CW_FADED_APRICOT),
-    METEORIC(ASTRONOMICAL, SColor.BRIGHT_GOLDEN_YELLOW),
-    VACUOUS(ASTRONOMICAL, SColor.DB_CERULEAN),
+    ASTRONOMICAL(SColor.CW_PALE_YELLOW), // a little bit of everything in space
+    SOLAR(ASTRONOMICAL, SColor.CW_LIGHT_GOLD), // The sun is just like any other star except it holds at least one being interested in "us"
+    LUNAR(ASTRONOMICAL, SColor.SILVER), // Tleco, the shut_tleco_ck-shaped Earth-like world, has a few moons that affect monsters in many ways
+    STELLAR(ASTRONOMICAL, SColor.BLUE_GREEN), // Stars may be home to god-like beings with no comprehension of mortals
+    COMETARY(ASTRONOMICAL, SColor.DB_PLATINUM), // Comets would be messages between celestial beings; this intercepts them
+    GRAVITY(ASTRONOMICAL, SColor.DB_STORM_CLOUD), // Drop it like it's hot, and then drop everything else on top of that
+    PLANETARY(ASTRONOMICAL, SColor.CW_FADED_APRICOT), // Knowledge of other planets and being able to mimic their conditions here
+    METEORIC(ASTRONOMICAL, SColor.BRIGHT_GOLDEN_YELLOW), // Meteors are space debris that falls to ground, this moves them to attack enemies
+    VACUOUS(ASTRONOMICAL, SColor.DB_CERULEAN), // EXPLOSIVE DECOMPRESSION COMMENCE
+    WARP(ASTRONOMICAL, SColor.RED_VIOLET), // teleporting bits and pieces of enemies and their equipment
     // Misc
     LIGHT(null, "light", "Basic lighting, not related to magical or other effects.", SColor.ALICE_BLUE),
     DEATH(SColor.SISKIN_SPROUT_YELLOW),
@@ -57,7 +63,7 @@ public enum Element { // TODO - make this a full EpiData class with external dat
     COLOR(SColor.INDIGO_WHITE);
 
     public Element parent;
-    public String name;
+    public String name, styledName;
     public String description;
     public Color color;
 
@@ -80,7 +86,16 @@ public enum Element { // TODO - make this a full EpiData class with external dat
     private Element(Element parent, String name, String description, Color color) {
         this.parent = parent;
         this.name = name == null || name.isEmpty() ? name().toLowerCase() : name;
+        this.styledName = "[#"+ StringKit.hex(Color.rgba8888(color)) + "]" + this.name + "[White]";
         this.description = description;
         this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    public String toStyledString() {
+        return styledName;
     }
 }
