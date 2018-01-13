c1900218117e503c1b616ef2de3d0e0e665feb3a
Eben Howard <SquidPony@users.noreply.github.com>
2018-01-12 23:08:23 -0800

    Crits work now; they can wiggle the target's glyph
    They also are more likely to hit (but not guaranteed) and do more
    damage, plus they print a different message when they hit, defeat, or
    miss a target.

--- Proto/core/src/main/java/squidpony/epigon/combat/ActionOutcome.java
+++ Proto/core/src/main/java/squidpony/epigon/combat/ActionOutcome.java
@@ -1,7 +1,12 @@
 package squidpony.epigon.combat;
 
-import squidpony.epigon.data.generic.Modification;
+import com.badlogic.gdx.math.MathUtils;
+import squidpony.epigon.GauntRNG;
 import squidpony.epigon.data.specific.Physical;
+import squidpony.squidmath.NumberTools;
+
+import static squidpony.epigon.data.specific.Physical.*;
+import static squidpony.squidmath.ThrustAltRNG.determine;
 
 /**
  * Handles the specific result of one event in a combat, such as whether a sword swing hit, how much damage it did, if
@@ -12,16 +17,29 @@ import squidpony.epigon.data.specific.Physical;
  * Created by Tommy Ettinger on 1/10/2018.
  */
 public class ActionOutcome {
-    public Modification actorChange, targetChange;
+    public boolean crit, hit, targetConditioned, actorConditioned;
+    public int attemptedDamage, actualDamage, actorDamage;
     public ActionOutcome()
     {
-        actorChange = new Modification();
-        targetChange = new Modification();
     }
-    // no-op currently
+
     public static ActionOutcome attack(Physical actor, Physical target)
     {
         ActionOutcome ao = new ActionOutcome();
+        long r = determine(++actor.chaos);
+        ao.crit = (20 + 4 * (actor.calcStats[CRIT] + actor.weaponData.calcStats[CRIT] -
+                target.calcStats[STEALTH] - target.weaponData.calcStats[STEALTH])) >= GauntRNG.next(r, 9);
+        ao.hit = (67 + 5 * ((ao.crit ? 2 : 0) + actor.calcStats[PRECISION] + actor.weaponData.calcStats[PRECISION] -
+                target.calcStats[EVASION] - target.weaponData.calcStats[EVASION])) >= GauntRNG.next(r + 1, 7);
+        if(ao.hit)
+        {
+            ao.attemptedDamage = Math.min(0, MathUtils.floor((NumberTools.randomFloatCurved(r + 2) * 0.4f - 0.45f) * ((ao.crit ? 2 : 1) +
+                    actor.calcStats[DAMAGE] + actor.weaponData.calcStats[DAMAGE])));
+            ao.actualDamage = Math.min(0, ao.attemptedDamage -
+                    MathUtils.floor(NumberTools.randomFloatCurved(r + 3) * 0.3f + 0.35f) * (target.calcStats[DEFENSE] + target.weaponData.calcStats[DEFENSE]));
+            ao.targetConditioned = (35 + 5 * ((ao.crit ? 1 : 0) + actor.calcStats[INFLUENCE] + actor.weaponData.calcStats[INFLUENCE] -
+                    target.calcStats[LUCK] - target.weaponData.calcStats[LUCK])) >= GauntRNG.next(r + 4, 8);
+        }
         return ao;
     }
 }
