84b9fa41e30376de33615937c238f0bcc3b9073c
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-04-07 20:45:23 -0700

    Work on some weapons, preparing for ranged combat
    Since the contents of Iosevka-Family-msdf are slightly different from
    Iosevka (non-family) as we had at the start, the full contents of the
    current font are in Redux/Iosevka-Family-contents-msdf.txt , and may
    need to be checked against the char listing in Utilities (probably in
    some automated way).
    
    PositionRNG is no longer a RandomnessSource (it was never used by RNG),
    just in case there was some polymorphic issue slowing down RNGs with
    different RandomnessSources.

--- Redux/core/src/main/java/squidpony/epigon/PositionRNG.java
+++ Redux/core/src/main/java/squidpony/epigon/PositionRNG.java
@@ -1,12 +1,11 @@
 package squidpony.epigon;
 
 import squidpony.squidmath.NumberTools;
-import squidpony.squidmath.RandomnessSource;
 
 /**
  * Created by Tommy Ettinger on 12/25/2017.
  */
-public class PositionRNG implements RandomnessSource
+public final class PositionRNG
 {
     public long stream;
     public long state = 1L;
@@ -22,17 +21,17 @@ public class PositionRNG implements RandomnessSource
         return ((x ^ x << 17) + (y ^ y >>> 23)) * 0x352E9CF570932BDDL; // multiply to reduce binary rank issue
     }
 
-    /**
-     * PCG-Random's RXS-M-XS output function on 64 bits of state with 64 bits of output
-     * @param state a state long produced by an LCG
-     * @return a higher-quality pseudo-random long
-     */
-    public static long pcg(final long state)
-    {
-        final long result = ((state >>> ((state >>> 59) + 5)) ^ state)
-                    * 0xAEF17502108EF2D9L;
-        return (result >>> 43) ^ result;
-    }
+//    /**
+//     * PCG-Random's RXS-M-XS output function on 64 bits of state with 64 bits of output
+//     * @param state a state long produced by an LCG
+//     * @return a higher-quality pseudo-random long
+//     */
+//    public static long pcg(final long state)
+//    {
+//        final long result = ((state >>> ((state >>> 59) + 5)) ^ state)
+//                    * 0xAEF17502108EF2D9L;
+//        return (result >>> 43) ^ result;
+//    }
 
     public PositionRNG()
     {
@@ -53,40 +52,37 @@ public class PositionRNG implements RandomnessSource
         this.stream = stream | 1L;
         this.state = state;
     }
-    public void move(long x, long y)
+    public final void move(long x, long y)
     {
         stream = hash2(x, y) | 1L;
         state = hash2(stream * x + y, stream * y + x) ^ stream;
     }
-    public void move(long seed, long x, long y)
+    public final void move(long seed, long x, long y)
     {
         stream = hash2(x, y) | 1L;
         state = seed;
     }
 
-    @Override
-    public int next(int bits) {
-        long z = (state = state * 0x5851F42D4C957F2DL + stream);
+    public final int next(final int bits) {
+        final long z = (state = state * 0x5851F42D4C957F2DL + stream);
         final long result = ((z >>> ((z >>> 59) + 5)) ^ z) * 0xAEF17502108EF2D9L;
         return (int)(((result >>> 43) ^ result) >>> (64 - bits));
     }
 
-    @Override
-    public long nextLong() {
-        long z = (state = state * 0x5851F42D4C957F2DL + stream);
+    public final long nextLong() {
+        final long z = (state = state * 0x5851F42D4C957F2DL + stream);
         final long result = ((z >>> ((z >>> 59) + 5)) ^ z) * 0xAEF17502108EF2D9L;
         return ((result >>> 43) ^ result);
     }
 
-    @Override
     public PositionRNG copy() {
         return new PositionRNG(state, stream);
     }
 
-    public float nextFloat() {
+    public final float nextFloat() {
         return NumberTools.formFloat((int)nextLong());
     }
-    public float nextFloat(float bound) {
+    public final float nextFloat(final float bound) {
         return bound * NumberTools.formFloat((int)nextLong());
     }
 }
