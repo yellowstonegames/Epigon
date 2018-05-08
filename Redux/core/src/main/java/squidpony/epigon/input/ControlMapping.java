6ef38f5e7d11690828e078e790b419036b99a32b
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-05-07 22:09:04 -0700

    Pretty big changes to input internals for remapping
    Remapping doesn't work yet, but the ground work is there and some key
    things have been adjusted so one set of key bindings can be used for the
    whole game instead of needing to rebind each screen type separately.

--- Redux/core/src/main/java/squidpony/epigon/input/ControlMapping.java
+++ Redux/core/src/main/java/squidpony/epigon/input/ControlMapping.java
@@ -1,6 +1,8 @@
 package squidpony.epigon.input;
 
+import squidpony.Maker;
 import squidpony.squidmath.OrderedMap;
+import squidpony.squidmath.UnorderedSet;
 
 import static squidpony.epigon.input.Verb.*;
 import static squidpony.squidgrid.gui.gdx.SquidInput.*;
@@ -11,7 +13,7 @@ import static squidpony.squidgrid.gui.gdx.SquidInput.*;
 public class ControlMapping {
 
     private static final int CAPS = 0x40000, CTRL = 0x20000;
-    public static final OrderedMap<Integer, Verb> defaultMapViewMapping = OrderedMap.makeMap(
+    public static final OrderedMap<Integer, Verb> allMappings = OrderedMap.makeMap(
             (int) 'A' | CAPS, ATTEMPT,
             (int) 'c', CONSUME,
             (int) 'c' | CTRL, CONSUME_WEIRDLY, // ctrl-c to eat the delicious soap
@@ -47,6 +49,7 @@ public class ControlMapping {
             (int) 'b', MOVE_DOWN_LEFT,
             (int) 'n', MOVE_DOWN_RIGHT,
             (int) '.', WAIT,
+            (int) CENTER_ARROW, WAIT,
             (int) '<' | CAPS, MOVE_HIGHER,
             (int) '>' | CAPS, MOVE_LOWER,
             (int) '[', CONTEXT_PRIOR,
@@ -57,86 +60,141 @@ public class ControlMapping {
             (int) 's', SHUT,
             (int) 'S' | CAPS, SAVE,
             (int) 'S' | CTRL, SAVE,
+            (int) 'T' | CAPS, TRY_AGAIN,
+            (int) 't', TRY_AGAIN,
             (int) 'Q' | CAPS, QUIT,
             (int) 'q' | CTRL, QUIT,
-            (int) ESCAPE, QUIT
+            (int) ESCAPE, CLOSE_SCREEN
     );
-    public static final OrderedMap<Integer, Verb> defaultEquipmentViewMapping = OrderedMap.makeMap(
-            (int) 'A' | CAPS, ATTEMPT,
-            (int) 'c', CONSUME,
-            (int) 'c' | CTRL, CONSUME_WEIRDLY, // ctrl-c to eat the delicious soap
-            (int) 'f', FIRE,
-            (int) '?' | CAPS, HELP,
-            (int) F1, HELP,
-            (int) 'i', INTERACT,
-            (int) 'd', DRAW,
-            (int) 'D' | CAPS, DROP,
-            (int) 'x', EXAMINE,
-            (int) UP_ARROW, MOVE_UP,
-            (int) DOWN_ARROW, MOVE_DOWN,
-            (int) LEFT_ARROW, MOVE_LEFT,
-            (int) RIGHT_ARROW, MOVE_RIGHT,
-            (int) 'h', MOVE_LEFT,
-            (int) 'j', MOVE_DOWN,
-            (int) 'k', MOVE_UP,
-            (int) 'l', MOVE_RIGHT,
-            (int) '<' | CAPS, UI_PREVIOUS_PAGE,
-            (int) '>' | CAPS, UI_NEXT_PAGE,
-            (int) PAGE_UP, MESSAGE_PRIOR,
-            (int) PAGE_DOWN, MESSAGE_NEXT,
-            (int) '[', CONTEXT_PRIOR,
-            (int) ']', CONTEXT_NEXT,
-            (int) '{' | CAPS, INFO_PRIOR,
-            (int) '}' | CAPS, INFO_NEXT,
-            (int) 'e', EQUIPMENT, // acts the same as closing the window while open
-            (int) 'Q' | CAPS, UI_CLOSE_WINDOW,
-            (int) 'q' | CTRL, UI_CLOSE_WINDOW,
-            (int) ESCAPE, UI_CLOSE_WINDOW
+
+
+    public static final UnorderedSet<Verb> defaultMapViewMapping = Maker.makeUOS(
+            ATTEMPT,
+            CONSUME,
+            CONSUME_WEIRDLY, // ctrl-c to eat the delicious soap
+            EQUIPMENT,
+            FIRE,
+            GET,
+            GATHER,
+            HELP,
+            HELP,
+            INTERACT,
+            DRAW,
+            DROP,
+            REST,
+            USE_POWER,
+            VIEW,
+            EXAMINE,
+            MOVE_UP,
+            MOVE_DOWN,
+            MOVE_LEFT,
+            MOVE_RIGHT,
+            MOVE_UP_LEFT,
+            MOVE_UP_RIGHT,
+            MOVE_DOWN_LEFT,
+            MOVE_DOWN_RIGHT,
+            MESSAGE_PRIOR,
+            MESSAGE_NEXT,
+            MOVE_LEFT,
+            MOVE_DOWN,
+            MOVE_UP,
+            MOVE_RIGHT,
+            MOVE_UP_LEFT,
+            MOVE_UP_RIGHT,
+            MOVE_DOWN_LEFT,
+            MOVE_DOWN_RIGHT,
+            WAIT,
+            MOVE_HIGHER,
+            MOVE_LOWER,
+            CONTEXT_PRIOR,
+            CONTEXT_NEXT,
+            INFO_PRIOR,
+            INFO_NEXT,
+            OPEN,
+            SHUT,
+            SAVE,
+            SAVE,
+            QUIT,
+            QUIT,
+            CLOSE_SCREEN
     );
-    public static final OrderedMap<Integer, Verb> defaultHelpViewMapping = OrderedMap.makeMap(
-            (int) UP_ARROW, MOVE_UP,
-            (int) DOWN_ARROW, MOVE_DOWN,
-            (int) LEFT_ARROW, MOVE_LEFT,
-            (int) RIGHT_ARROW, MOVE_RIGHT,
-            (int) 'h', MOVE_LEFT,
-            (int) 'j', MOVE_DOWN,
-            (int) 'k', MOVE_UP,
-            (int) 'l', MOVE_RIGHT,
-            (int) PAGE_UP, MESSAGE_PRIOR,
-            (int) PAGE_DOWN, MESSAGE_NEXT,
-            (int) '[', CONTEXT_PRIOR,
-            (int) ']', CONTEXT_NEXT,
-            (int) '{' | CAPS, INFO_PRIOR,
-            (int) '}' | CAPS, INFO_NEXT,
-            (int) 'e', EQUIPMENT,
-            (int) '<' | CAPS, UI_PREVIOUS_PAGE,
-            (int) '>' | CAPS, UI_NEXT_PAGE,
-            (int) 'Q' | CAPS, UI_CLOSE_WINDOW,
-            (int) 'q' | CTRL, UI_CLOSE_WINDOW,
-            (int) ESCAPE, UI_CLOSE_WINDOW
+
+    public static final UnorderedSet<Verb> defaultEquipmentViewMapping = Maker.makeUOS(
+            ATTEMPT,
+            CONSUME,
+            CONSUME_WEIRDLY, // ctrl-c to eat the delicious soap
+            EQUIPMENT, // acts the same as closing the window while open
+            FIRE,
+            HELP,
+            HELP,
+            INTERACT,
+            DRAW,
+            DROP,
+            EXAMINE,
+            MOVE_UP,
+            MOVE_DOWN,
+            MOVE_LEFT,
+            MOVE_RIGHT,
+            MOVE_LEFT,
+            MOVE_DOWN,
+            MOVE_UP,
+            MOVE_RIGHT,
+            MOVE_HIGHER,
+            MOVE_LOWER,
+            MESSAGE_PRIOR,
+            MESSAGE_NEXT,
+            CONTEXT_PRIOR,
+            CONTEXT_NEXT,
+            INFO_PRIOR,
+            INFO_NEXT,
+            QUIT,
+            QUIT,
+            CLOSE_SCREEN
     );
-    public static final OrderedMap<Integer, Verb> defaultFallingViewMapping = OrderedMap.makeMap(
-            (int) UP_ARROW, MOVE_UP,
-            (int) DOWN_ARROW, MOVE_DOWN,
-            (int) LEFT_ARROW, MOVE_LEFT,
-            (int) RIGHT_ARROW, MOVE_RIGHT,
-            (int) 'h', MOVE_LEFT,
-            (int) 'j', MOVE_DOWN,
-            (int) 'k', MOVE_UP,
-            (int) 'l', MOVE_RIGHT,
-            (int) ' ', PAUSE,
-            (int) 'S' | CAPS, SAVE,
-            (int) 'S' | CTRL, SAVE,
-            (int) 'Q' | CAPS, QUIT,
-            (int) 'q' | CTRL, QUIT,
-            (int) ESCAPE, QUIT
+    public static final UnorderedSet<Verb> defaultHelpViewMapping = Maker.makeUOS(
+            MOVE_UP,
+            MOVE_DOWN,
+            MOVE_LEFT,
+            MOVE_RIGHT,
+            MOVE_LEFT,
+            MOVE_DOWN,
+            MOVE_UP,
+            MOVE_RIGHT,
+            MESSAGE_PRIOR,
+            MESSAGE_NEXT,
+            CONTEXT_PRIOR,
+            CONTEXT_NEXT,
+            INFO_PRIOR,
+            INFO_NEXT,
+            EQUIPMENT,
+            MOVE_HIGHER,
+            MOVE_LOWER,
+            QUIT,
+            QUIT,
+            CLOSE_SCREEN
     );
-    public static final OrderedMap<Integer, Verb> defaultFallingViewGameOverMapping = OrderedMap.makeMap(
-            (int) 'r' | CAPS, REPLAY,
-            (int) 'r', REPLAY,
-            (int) 'Q' | CAPS, QUIT,
-            (int) 'q' | CTRL, QUIT,
-            (int) 'q', QUIT,
-            (int) ESCAPE, QUIT
+    public static final UnorderedSet<Verb> defaultFallingViewMapping = Maker.makeUOS(
+            MOVE_UP,
+            MOVE_DOWN,
+            MOVE_LEFT,
+            MOVE_RIGHT,
+            MOVE_LEFT,
+            MOVE_DOWN,
+            MOVE_UP,
+            MOVE_RIGHT,
+            PAUSE,
+            SAVE,
+            SAVE,
+            QUIT,
+            QUIT,
+            QUIT
+    );
+    public static final UnorderedSet<Verb> defaultFallingViewGameOverMapping = Maker.makeUOS(
+            TRY_AGAIN,
+            TRY_AGAIN,
+            QUIT,
+            QUIT,
+            QUIT,
+            QUIT
     );
 }
