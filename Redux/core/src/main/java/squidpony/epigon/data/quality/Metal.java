3e4797e4138fc3a79ba4c72a22792572d85f40ba
Tommy Ettinger <tommy.ettinger@gmail.com>
2018-05-23 22:48:02 -0700

    More content for materials

--- Redux/core/src/main/java/squidpony/epigon/data/quality/Metal.java
+++ Redux/core/src/main/java/squidpony/epigon/data/quality/Metal.java
@@ -9,14 +9,20 @@ import squidpony.squidgrid.gui.gdx.SColor;
  */
 public enum Metal implements Material {
     BRASS(SColor.BRASS, SColor.CW_GOLD, 170, 1600),
+    BRONZE(SColor.BRONZE, SColor.CW_DRAB_CYAN, 180, 2000),
     LEAD(SColor.DB_LEAD, SColor.DB_LEAD, 110, 700),
     COPPER(SColor.COPPER, SColor.COPPER_ROSE, 230, 1100),
     SILVER(SColor.SILVER, SColor.SILVER, 500, 1000),
-    GOLD(SColor.CW_BRIGHT_GOLD, SColor.OLD_GOLD, 600, 700),
+    GOLD(SColor.CW_BRIGHT_GOLD, SColor.OLD_GOLD, 700, 700),
     TIN(SColor.CW_GRAY_WHITE, SColor.CW_GRAY_WHITE, 140, 1100),
     IRON(SColor.DB_IRON, SColor.DB_IRON, 250, 2500),
     STEEL(SColor.CW_GRAY_WHITE, SColor.STEEL_BLUE, 310, 2900),
-    PLATINUM(SColor.PLATINUM, SColor.DB_PLATINUM, 750, 1900);
+    PLATINUM(SColor.PLATINUM, SColor.DB_PLATINUM, 750, 1900),
+    // Fantasy metals
+    MITHRIL(SColor.DB_PLATINUM, SColor.INDIGO_WHITE, 900, 4000),
+    ADAMANTINE(SColor.PURE_DARK_GRAY, SColor.DB_GRAPHITE, 1300, 9000),
+    INK_ZINC(SColor.PERSIAN_INDIGO, SColor.ROYAL_PURPLE, 1500, 3500),
+    DAMASC_STEEL(SColor.CW_GRAY_WHITE, SColor.CW_GRAY, 550, 3400);
 
     public Color front, back;
     public int value; //base material is 100, metals are 200
