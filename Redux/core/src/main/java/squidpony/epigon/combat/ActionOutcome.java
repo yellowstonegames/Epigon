e8104d43618395797c0e01f8b5cb0dde5348d53e
Eben Howard <SquidPony@users.noreply.github.com>
2018-05-25 23:00:09 -0700

    Support dual-wielding, range checks
    Dual-wielding is extremely inaccurate, with a roughly 60% failure chance
    for your left and right weapons if you also fight unarmed (your unarmed
    attacks may also hit, but have an 80% failure chance in this case; all
    attacks still may miss normally). But, you can have a one-handed
    implement and a sword or something and attack at both range and melee,
    without risking dual-wield miss chances. This may need balancing!

--- Redux/core/src/main/java/squidpony/epigon/combat/ActionOutcome.java
+++ Redux/core/src/main/java/squidpony/epigon/combat/ActionOutcome.java
@@ -2,8 +2,11 @@ package squidpony.epigon.combat;
 
 import squidpony.epigon.data.*;
 import squidpony.epigon.data.quality.Element;
+import squidpony.squidgrid.Radius;
 import squidpony.squidmath.Noise;
 import squidpony.squidmath.NumberTools;
+
+import java.util.ArrayList;
 
 /**
  * Handles the specific result of one event in a combat, such as whether a sword swing hit, how much damage it did, if
@@ -16,66 +19,80 @@ import squidpony.squidmath.NumberTools;
 public class ActionOutcome {
     public boolean crit, hit, targetConditioned, actorConditioned;
     public int attemptedDamage, actualDamage, actorDamage;
-    public Weapon actorWeapon, targetWeapon;
+    public Weapon targetWeapon;
+    public Weapon actorWeapon;
     public Element element;
     public String actorCondition, targetCondition;
     public ActionOutcome()
     {
     }
-    public static ActionOutcome attack(Physical actor, Physical target)
+    public static ArrayList<ActionOutcome> attack(Physical actor, Physical target)
     {
-        ActionOutcome ao = new ActionOutcome();
-        ao.actorWeapon = actor.creatureData == null ? Weapon.randomUnarmedWeapon(actor) : actor.creatureData.weaponChoices.random();
-        int index = ao.actorWeapon.elements.table.random(actor.nextLong());
-        ao.element = ao.actorWeapon.elements.items.get(index);
-        ao.targetCondition = index < ao.actorWeapon.statuses.size() ? ao.actorWeapon.statuses.get(index) : actor.getRandomElement(ao.actorWeapon.statuses);
-        ao.targetWeapon = target.creatureData == null ? Weapon.randomUnarmedWeapon(actor) : target.creatureData.weaponChoices.random();
-        int actorSkill = 1, targetSkill = 1;
-        if(actor.creatureData != null)
-        {
-            for (int i = 0; i < ao.actorWeapon.skills.length; i++) {
-                actorSkill += actor.creatureData.skills.getOrDefault(ao.actorWeapon.skills[i], Rating.NONE).ordinal();
+        ArrayList<ActionOutcome> aos = new ArrayList<>(3);
+        if(actor.creatureData != null) {
+            int totalWeight = 0;
+            for (int i = 0; i < actor.creatureData.weaponChoices.table.size(); i++) {
+                ActionOutcome ao = new ActionOutcome();
+                Weapon w = actor.creatureData.weaponChoices.table.keyAt(i);
+                if(Radius.CIRCLE.radius(actor.location, target.location) > w.rawWeapon.range + 1)
+                    continue;
+                ao.actorWeapon = w;
+                totalWeight += actor.creatureData.weaponChoices.weight(w);
+                aos.add(ao);
             }
-        }
-        if(target.creatureData != null)
-        {
-            for (int i = 0; i < ao.targetWeapon.skills.length; i++) {
-                targetSkill += target.creatureData.skills.getOrDefault(ao.targetWeapon.skills[i], Rating.NONE).ordinal();
-            }
-        }
-        actor.statEffects.add(ao.actorWeapon.calcStats);
-        target.statEffects.add(ao.targetWeapon.calcStats);
-        ChangeTable.holdPhysical(actor, actor.statEffects);
-        //System.out.println("Attacker is " + actor.name + " with base stats " + actor.stats + " and adjusted stats: " + tempActorStats);
-        ChangeTable.holdPhysical(target, target.statEffects);
-        //System.out.println("Defender is " + target.name  + " with base stats " + target.stats + " and adjusted stats: " + tempTargetStats);
-        
-        ao.crit = (5 + (actor.actualStat(CalcStat.CRIT) - target.actualStat(CalcStat.STEALTH))) >= actor.nextInt(50);
-        double actorPrecision = actor.actualStat(CalcStat.PRECISION) + actorSkill,
-                targetEvasion = target.actualStat(CalcStat.EVASION) + targetSkill;
-        ao.hit = (67 + 5 * ((ao.crit ? 2 : 0) + actorPrecision - targetEvasion)) >= actor.next(7);
+            for (int wi = 0; wi < aos.size(); wi++) {
+                ActionOutcome ao = aos.get(wi);
+                Weapon w = ao.actorWeapon;
+                int index = w.elements.table.random(actor.nextLong());
+                ao.element = w.elements.items.get(index);
+                ao.targetCondition = index < w.statuses.size() ? w.statuses.get(index) : actor.getRandomElement(w.statuses);
+                ao.targetWeapon = target.creatureData == null ? Weapon.randomUnarmedWeapon(actor) : target.creatureData.weaponChoices.random();
+                if(actor.nextInt(totalWeight) >= actor.creatureData.weaponChoices.weight(w))
+                    continue;
+                int actorSkill = 1, targetSkill = 1;
+                if (actor.creatureData != null) {
+                    for (int i = 0; i < w.skills.length; i++) {
+                        actorSkill += actor.creatureData.skills.getOrDefault(w.skills[i], Rating.NONE).ordinal();
+                    }
+                }
+                if (target.creatureData != null) {
+                    for (int i = 0; i < ao.targetWeapon.skills.length; i++) {
+                        targetSkill += target.creatureData.skills.getOrDefault(ao.targetWeapon.skills[i], Rating.NONE).ordinal();
+                    }
+                }
+                actor.statEffects.add(w.calcStats);
+                target.statEffects.add(ao.targetWeapon.calcStats);
+                ChangeTable.holdPhysical(actor, actor.statEffects);
+                //System.out.println("Attacker is " + actor.name + " with base stats " + actor.stats + " and adjusted stats: " + tempActorStats);
+                ChangeTable.holdPhysical(target, target.statEffects);
+                //System.out.println("Defender is " + target.name  + " with base stats " + target.stats + " and adjusted stats: " + tempTargetStats);
+
+                ao.crit = (5 + (actor.actualStat(CalcStat.CRIT) - target.actualStat(CalcStat.STEALTH))) >= actor.nextInt(50);
+                double actorPrecision = actor.actualStat(CalcStat.PRECISION) + actorSkill,
+                        targetEvasion = target.actualStat(CalcStat.EVASION) + targetSkill;
+                ao.hit = (67 + 5 * ((ao.crit ? 2 : 0) + actorPrecision - targetEvasion)) >= actor.next(7);
 //        ao.hit = (67 + 5 * ((ao.crit ? 2 : 0) + actor.stats.getOrDefault(CalcStat.PRECISION, LiveValue.ZERO).actual() -
 //                target.stats.getOrDefault(CalcStat.EVASION, LiveValue.ZERO).actual())) >= GauntRNG.next(r + 1, 7);
-        if(ao.hit)
-        {
-            ao.attemptedDamage = Math.min(0, Noise.fastFloor((NumberTools.formCurvedFloat(actor.nextLong()) * 0.4f - 0.45f) * ((ao.crit ? 2 : 1) +
-                    actor.actualStat(CalcStat.DAMAGE) + actorSkill)));
-            ao.actualDamage = Math.min(0, ao.attemptedDamage -
-                    Noise.fastFloor((NumberTools.formCurvedFloat(actor.nextLong()) * 0.3f + 0.35f) * (target.actualStat(CalcStat.DEFENSE) +  targetSkill)));
-            ao.targetConditioned = (35 + 5 * ((ao.crit ? 1 : 0) + actor.actualStat(CalcStat.INFLUENCE) + actorSkill -
-                    target.actualStat(CalcStat.LUCK) - targetSkill)) >= actor.next(8);
+                if (ao.hit) {
+                    ao.attemptedDamage = Math.min(0, Noise.fastFloor((NumberTools.formCurvedFloat(actor.nextLong()) * 0.4f - 0.45f) * ((ao.crit ? 2 : 1) +
+                            actor.actualStat(CalcStat.DAMAGE) + actorSkill)));
+                    ao.actualDamage = Math.min(0, ao.attemptedDamage -
+                            Noise.fastFloor((NumberTools.formCurvedFloat(actor.nextLong()) * 0.3f + 0.35f) * (target.actualStat(CalcStat.DEFENSE) + targetSkill)));
+                    ao.targetConditioned = (35 + 5 * ((ao.crit ? 1 : 0) + actor.actualStat(CalcStat.INFLUENCE) + actorSkill -
+                            target.actualStat(CalcStat.LUCK) - targetSkill)) >= actor.next(8);
+                }
+                ChangeTable.releasePhysical(actor, actor.statEffects);
+                ChangeTable.releasePhysical(target, target.statEffects);
+                actor.statEffects.removeLast();
+                target.statEffects.removeLast();
+                if (ao.targetConditioned) {
+                    Condition c = new Condition(ConditionBlueprint.CONDITIONS.getOrDefault(ao.targetCondition, ConditionBlueprint.CONDITIONS.getAt(0)), target, ao.element);
+                    ChangeTable.strikePhysical(target, c.parent.changes);
+                    target.conditions.add(c);
+
+                }
+            }
         }
-        ChangeTable.releasePhysical(actor, actor.statEffects);
-        ChangeTable.releasePhysical(target, target.statEffects);
-        actor.statEffects.removeLast();
-        target.statEffects.removeLast();
-        if(ao.targetConditioned)
-        {
-            Condition c = new Condition(ConditionBlueprint.CONDITIONS.getOrDefault(ao.targetCondition, ConditionBlueprint.CONDITIONS.getAt(0)), target, ao.element);
-            ChangeTable.strikePhysical(target, c.parent.changes);
-            target.conditions.add(c);
-            
-        }
-        return ao;
+        return aos;
     }
 }
