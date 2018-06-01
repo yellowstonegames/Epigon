43fe829436422aafd349c6b1cfa182273e0ab405
Eben Howard <SquidPony@users.noreply.github.com>
2018-05-31 22:49:41 -0700

    Switch PositionRNG to use Mizuchi variant
    Mizuchi is just LinnormRNG with a stream it adds instead of always adding 1 in its first step; it's in Sarong but not SquidLib yet. This selects the stream using a slight variant on Linnorm/Mizuchi to randomize the influence of x and y jointly when the PositionRNG moves. Notably, this doesn't set the state to a number based on stream anymore if you call move(long, long); it will always use a state of 0, which seems good in testing. A few of the tests show a decent improvement over the old way, but most are similar in quality (the new way should be faster since Mizuchi is almost exactly as fast as Linnorm, which is our fastest good generator, in Sarong's benchmarks).

---
+++ Redux/core/src/test/java/squidpony/epigon/playground/PositionRNGOld.java
@@ -0,0 +1,88 @@
+package squidpony.epigon.playground;
+
+import squidpony.squidmath.NumberTools;
+
+/**
+ * Created by Tommy Ettinger on 12/25/2017.
+ */
+public final class PositionRNGOld
+{
+    public long stream;
+    public long state = 1L;
+
+    private static long hash2(long x, long y)
+    {
+        x += 0x6C8E9CD570932BD5L; // increase weight, usually
+        y -= 0x6C8E9CD570932BD5L;
+        x ^= x << 13;
+        y ^= y << 33;
+        x ^= x >>> 7;
+        y ^= y << 14;
+        return ((x ^ x << 17) + (y ^ y >>> 23)) * 0x352E9CF570932BDDL; // multiply to reduce binary rank issue
+    }
+
+//    /**
+//     * PCG-Random's RXS-M-XS output function on 64 bits of state with 64 bits of output
+//     * @param state a state long produced by an LCG
+//     * @return a higher-quality pseudo-random long
+//     */
+//    public static long pcg(final long state)
+//    {
+//        final long result = ((state >>> ((state >>> 59) + 5)) ^ state)
+//                    * 0xAEF17502108EF2D9L;
+//        return (result >>> 43) ^ result;
+//    }
+
+    public PositionRNGOld()
+    {
+        this(1L);
+    }
+
+    public PositionRNGOld(long seed)
+    {
+        this(seed, 0L, 0L);
+    }
+    public PositionRNGOld(long seed, long x, long y)
+    {
+        stream = hash2(x, y) | 1L;
+        state = seed;
+    }
+    public PositionRNGOld(long state, long stream)
+    {
+        this.stream = stream | 1L;
+        this.state = state;
+    }
+    public final void move(long x, long y)
+    {
+        stream = hash2(x, y) | 1L;
+        state = hash2(stream * x + y, stream * y + x) ^ stream;
+    }
+    public final void move(long seed, long x, long y)
+    {
+        stream = hash2(x, y) | 1L;
+        state = seed;
+    }
+
+    public final int next(final int bits) {
+        final long z = (state = state * 0x5851F42D4C957F2DL + stream);
+        final long result = ((z >>> ((z >>> 59) + 5)) ^ z) * 0xAEF17502108EF2D9L;
+        return (int)(((result >>> 43) ^ result) >>> (64 - bits));
+    }
+
+    public final long nextLong() {
+        final long z = (state = state * 0x5851F42D4C957F2DL + stream);
+        final long result = ((z >>> ((z >>> 59) + 5)) ^ z) * 0xAEF17502108EF2D9L;
+        return ((result >>> 43) ^ result);
+    }
+
+    public PositionRNGOld copy() {
+        return new PositionRNGOld(state, stream);
+    }
+
+    public final float nextFloat() {
+        return NumberTools.formFloat((int)nextLong());
+    }
+    public final float nextFloat(final float bound) {
+        return bound * NumberTools.formFloat((int)nextLong());
+    }
+}
--- Redux/core/src/test/java/squidpony/epigon/playground/PositionRNGTest.java
+++ Redux/core/src/test/java/squidpony/epigon/playground/PositionRNGTest.java
@@ -1,5 +1,6 @@
 package squidpony.epigon.playground;
 
+import squidpony.StringKit;
 import squidpony.epigon.PositionRNG;
 
 /**
@@ -8,33 +9,76 @@ import squidpony.epigon.PositionRNG;
 public class PositionRNGTest {
     public static void main(String[] args)
     {
+        testBit0();
+        System.out.println();
         testBitPair();
+        System.out.println("\nAnd old way:\n");
+        testBit0Old();
+        System.out.println();
+        testBitPairOld();
     }
     public static void testBit0()
     {
         PositionRNG p = new PositionRNG(12345L);
+        int total = 0, v;
         for (int y = 0; y < 20; y++) {
             for (int x = 0; x < 20; x++) {
                 p.move(x, y);
-                System.out.printf("%1d ", p.next(1));
-            }
-            System.out.println();
-        }
-    }
-    public static void testBitPair()
-    {
-        PositionRNG p = new PositionRNG(12345L);
-        PositionRNG p2 = new PositionRNG(0L);
-        int i = 0, total = 0;
-        for (int y = 0; y < 20; y++) {
-            for (int x = 0; x < 20; x++) {
-                p.move(x, y);
-                p2.move(1000000000L + p.stream, x, y);
-                System.out.printf("%1d ", i = (p.next(1) + p2.next(1)));
-                total += i;
+                total += (v = p.next(1));
+                System.out.printf("%1d ", v);
             }
             System.out.println();
         }
         System.out.println(total);
+    }
+    public static void testBitPair()
+    {
+        PositionRNG p = new PositionRNG();
+        PositionRNG p2 = new PositionRNG();
+        int i = 0, total = 0;
+        int[] is = new int[9];
+        for (int y = 0; y < 48; y++) {
+            for (int x = 0; x < 48; x++) {
+                p.move(x, y);
+                p2.move(111L, x, y);
+                System.out.printf("%1d ", i = (p.next(3) + p2.next(1)));
+                is[i]++;
+                total += i;
+            }
+            System.out.println();
+        }
+        System.out.println(total + " total, individual rates " + StringKit.join(", ", is));
+    }
+    public static void testBit0Old()
+    {
+        PositionRNGOld p = new PositionRNGOld(12345L);
+        int total = 0, v;
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                p.move(x, y);
+                total += (v = p.next(1));
+                System.out.printf("%1d ", v);
+            }
+            System.out.println();
+        }
+        System.out.println(total);
+    }
+    public static void testBitPairOld()
+    {
+        PositionRNGOld p = new PositionRNGOld();
+        PositionRNGOld p2 = new PositionRNGOld();
+        int i = 0, total = 0;
+        int[] is = new int[9];
+        for (int y = 0; y < 48; y++) {
+            for (int x = 0; x < 48; x++) {
+                p.move(x, y);
+                p2.move(111L, x, y);
+                System.out.printf("%1d ", i = (p.next(3) + p2.next(1)));
+                is[i]++;
+                total += i;
+            }
+            System.out.println();
+        }
+        System.out.println(total + " total, individual rates " + StringKit.join(", ", is));
     }
 }
