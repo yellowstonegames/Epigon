a080e7a2d6c9ec2c21fde08767fc8a3badad7c62
Eben Howard <SquidPony@users.noreply.github.com>
2018-10-04 07:28:52 -0700

    adding a few real world castle details for reference later

--- core/src/main/java/squidpony/epigon/mapping/Castle.java
+++ core/src/main/java/squidpony/epigon/mapping/Castle.java
@@ -9,15 +9,36 @@ import squidpony.squidmath.GreasedRegion;
  */
 public class Castle {
     /* Castle notes from real castles
-    Average thickness of stone wall: 2m
-    Thickness of Chepstow Castle in Wales: 6m
-    Thickness of some walls at Borl Castle in Coratia: 12m
-    Length of wall at Conwy Castle in Wales: 1280m
+    Average thickness of stone wall: 2m but can be up to 15m
 
-    Standard motte and baily area: 3 acres
-    Malbork Castle total area: 12 square km
+    Standard motte and baily area: 0.12 square km
 
-    Approximate largest keeps: 31m x 31m
+    Approximate largest stone keeps: 31m x 31m
+
+    Motte and Bailey style usually had one of each but sometimes up to two
+    Motte is a raised mound with a building on it for final defense
+    Mottes were 5-15m high (just the earthen part, not including the building on top)
+    Bailey is the larger, lower open area surrounded by a wall or pallisagde
+    A drawbridge connected the bailey to the motte
+
+    Shell Keeps were essentially a Bailey only with a stone wall instead of wooden pallisades
+
+    Pleshey Castle (Motte & Bailey style, 1 each)
+      - bailey is a kidney shape 190m x 90m
+      - base of motte is 90m diameter
+      - top of motte is 28m
+      - motte is 15m high
+      - had wood pallisades and buildings
+
+    Malbork Castle
+      - total area covered, including grounds outside walls: 12 square km
+
+    Borl Castle (in Croatia)
+      - walls thickness up to 12m
+
+    Chepstow Castle (in Wales)
+      - walls thickness is 6m
+    
      */
     
     public EpiMap[] buildZone;
