05fa7aab28d04dead01950fb0b9196594d813e99
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-05-12 20:58:09 -0700

    Trying various alternatives to hashing objects in MapMemoryTest
    Using simple bit-sections for 32-bit x and 32-bit y in a 64-bit long key works fairly well, but Szudzik's pairing function seems to have a lot less collisions and is still recoverable (see unSzudzik()).

--- desktop/src/test/java/squidpony/epigon/playground/tests/MapMemoryTest.java
+++ desktop/src/test/java/squidpony/epigon/playground/tests/MapMemoryTest.java
@@ -5,30 +5,70 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.GridPoint2;
-import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.ObjectSet;
+
+import com.badlogic.gdx.math.Vector2;
 
 public class MapMemoryTest extends ApplicationAdapter {
-    private static final int width = 180, height = 180;
+    private static final int width = 18, height = 10;
 
     private static final int cellWidth = 1, cellHeight = 1;
-    // the initial bug was reported on ObjectMap, so that's what this uses (even though ObjectIntMap would be better)
-    private ObjectMap<GridPoint2, Integer> theMap;
+    // the initial bug was reported on ObjectMap
+//    private ObjectMap<GridPoint2, Integer> theMap;
+    private ObjectSet<Object> theMap;
 
     @Override
     public void create() {
-        theMap = new ObjectMap<>();
+        theMap = new ObjectSet<>();
         generate();
     }
 
+    private static long szudzik(long x, long y) {
+        x = (x << 1) ^ (x >> 63);
+        y = (y << 1) ^ (y >> 63);
+        return (x >= y ? x * x + x + y : x + y * y);
+    }
+
+    private static void unSzudzik(long[] output, long z) {
+        final long low = (long)Math.sqrt(z), lessSquare = z - low * low, x, y;
+        if(lessSquare < low) { 
+            x = lessSquare;
+            y = low;
+        }
+        else {
+            x = low;
+            y = lessSquare - low;
+        }
+        output[0] = x >> 1 ^ -(x & 1L);
+        output[1] = y >> 1 ^ -(y & 1L);
+    }
+
+
     public void generate()
     {
+//        long[] pair = new long[2];
         System.out.println("Initial heap memory used: " + Gdx.app.getJavaHeap());
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                final GridPoint2 gp = new GridPoint2(x, y);
-                final int gpHash = gp.hashCode(); // uses the updated GridPoint2 hashCode(), not the current GDX code
-                theMap.put(gp, gpHash | 0xFF000000); //value doesn't matter; this was supposed to test ObjectMap
+        for (int x = -width; x < width; x++) {
+            for (int y = -height; y < height; y++) {
+//                long z = (x & 0xFFFFFFFFL) << 32 | (y & 0xFFFFFFFFL);
+//                z =        ((z & 0x00000000ffff0000L) << 16) | ((z >>> 16) & 0x00000000ffff0000L) | (z & 0xffff00000000ffffL);
+//                z =        ((z & 0x0000ff000000ff00L) << 8 ) | ((z >>> 8 ) & 0x0000ff000000ff00L) | (z & 0xff0000ffff0000ffL);
+//                z =        ((z & 0x00f000f000f000f0L) << 4 ) | ((z >>> 4 ) & 0x00f000f000f000f0L) | (z & 0xf00ff00ff00ff00fL);
+//                z =        ((z & 0x0c0c0c0c0c0c0c0cL) << 2 ) | ((z >>> 2 ) & 0x0c0c0c0c0c0c0c0cL) | (z & 0xc3c3c3c3c3c3c3c3L);
+//                z =        ((z & 0x2222222222222222L) << 1 ) | ((z >>> 1 ) & 0x2222222222222222L) | (z & 0x9999999999999999L);
+//                theMap.put(z, null);                                                 // uses 23312536 bytes of heap
+//                long z = szudzik(x, y);
+//                theMap.put(z, null);                                                   // uses 18331216 bytes of heap?
+//                unSzudzik(pair, z);
+//                theMap.put(0xC13FA9A902A6328FL * x ^ 0x91E10DA5C79E7B1DL * y, null); // uses 23312576 bytes of heap
+//                theMap.put((x & 0xFFFFFFFFL) << 32 | (y & 0xFFFFFFFFL), null);       // uses 28555456 bytes of heap
+                theMap.add(new Vector2(x - width * 0.5f, y - height * 0.5f)); // crashes out of heap with 720 Vector2
+//                theMap.add(new GridPoint2(x, y));
+            }
+        }
+//                final GridPoint2 gp = new GridPoint2(x, y);
+//                final int gpHash = gp.hashCode(); // uses the updated GridPoint2 hashCode(), not the current GDX code
+//                theMap.put(gp, gpHash | 0xFF000000); //value doesn't matter; this was supposed to test ObjectMap
                 //theMap.put(gp, (53 * 53 + x + 53 * y) | 0xFF000000); //this is what the hashCodes would look like for the current code
                 
                 //final int gpHash = x * 0xC13F + y * 0x91E1; // updated hashCode()
@@ -36,8 +76,6 @@ public class MapMemoryTest extends ApplicationAdapter {
                 //// like the golden ratio but with better properties for 2D spaces. These don't need to be prime.
                 
                 //final int gpHash = 53 * 53 + x + 53 * y; // equivalent to current hashCode()
-            }
-        }
         System.out.println("Post-assign memory used: " + Gdx.app.getJavaHeap());
     }
 
