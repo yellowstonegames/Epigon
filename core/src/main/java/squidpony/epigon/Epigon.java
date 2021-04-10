76421b9f2019c7c9e31eb6b7fdeeed4e050cd313
Eben Howard <SquidPony@users.noreply.github.com>
2021-04-10 11:58:40 -0600

    move map drawing functionality into Epigon in prep for further refactoring

--- core/src/main/java/squidpony/epigon/Epigon.java
+++ core/src/main/java/squidpony/epigon/Epigon.java
@@ -2,9 +2,12 @@ package squidpony.epigon;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.profiling.GLProfiler;
+import com.badlogic.gdx.math.Frustum;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -1578,16 +1581,19 @@ public class Epigon extends Game {
             if (mapOverlaySLayers.isVisible()) {
                 mapOverlayStage.act();
                 mapOverlayHandler.updateDisplay();
-                if(mapOverlayHandler.getSubselection() != null)
+                if (mapOverlayHandler.getSubselection() != null) {
                     showInteractOptions(mapOverlayHandler.getSelected(), player, mapOverlayHandler.getSelection(), map);
+                }
                 batch.setProjectionMatrix(mapOverlayStage.getCamera().combined);
                 mapOverlayStage.getRoot().draw(batch, 1f);
+            } else {
+                //mapSLayers.draw(batch, 1f);
+                //passiveSLayers.draw(batch, 1f);
+                drawMap(mapSLayers, batch, 1f);
+                drawMap(passiveSLayers, batch, 1f);
             }
-            else {
-                mapSLayers.draw(batch, 1f);
-                passiveSLayers.draw(batch, 1f);
-            }
-            mapHoverSLayers.draw(batch, 1f);
+            //mapHoverSLayers.draw(batch, 1f);
+            drawMap(mapHoverSLayers, batch, 1f);
             batch.end();
         } else {
             //here we apply the other viewport, which clips a different area while leaving the message area intact.
@@ -1608,10 +1614,109 @@ public class Epigon extends Game {
         //this needs vsync set to false in DesktopLauncher.
         Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() + " FPS");
     }
+    
+    private void drawMap(SparseLayers layers, Batch batch, float parentAlpha) { // replacing SquareSparseLayers.java
+        //super.draw(batch, parentAlpha);
+        float xo = layers.getX(), yo = layers.getY(), yOff = yo + 1f + layers.gridHeight * font.actualCellHeight, gxo, gyo,
+            conditionY = 1f, conditionCw = 1f, conditionCm = 1f,
+            conditionYAdd = 0f, conditionCwAdd = 0f, conditionCmAdd = 0f;
+        Physical player = this.player;
+        float[][] walls = this.walls;
+        if (player.visualCondition == null) {
+            final int clen = player.conditions.size();
+            for (int i = clen - 1; i >= 0; i--) {
+                VisualCondition vis = player.conditions.getAt(i).parent.visual;
+                if (vis != null) {
+                    vis.update();
+                    conditionY *= vis.lumaMul;
+                    conditionCw *= vis.warmMul;
+                    conditionCm *= vis.mildMul;
+                    conditionYAdd += vis.lumaAdd;
+                    conditionCwAdd += vis.warmAdd;
+                    conditionCmAdd += vis.mildAdd;
+                    break;
+                }
+            }
+        } else {
+            VisualCondition vis = player.visualCondition;
+            vis.update();
+            conditionY *= vis.lumaMul;
+            conditionCw *= vis.warmMul;
+            conditionCm *= vis.mildMul;
+            conditionYAdd += vis.lumaAdd;
+            conditionCwAdd += vis.warmAdd;
+            conditionCmAdd += vis.mildAdd;
+        }
+        Epigon.filter.yMul = 0.7f * conditionY;
+        Epigon.filter.cwMul = 0.65f * conditionCw;
+        Epigon.filter.cmMul = 0.65f * conditionCm;
+        Epigon.filter.yAdd = 0.7f * conditionYAdd;
+        Epigon.filter.cwAdd = 0.65f * conditionCwAdd;
+        Epigon.filter.cmAdd = 0.65f * conditionCmAdd;
+//                font.draw(batch, backgrounds, xo, yo);
+        font.draw(batch, layers.backgrounds, xo - font.actualCellWidth * 0.25f, yo);
+        int len = layers.layers.size();
+        Frustum frustum = null;
+        Stage stage = layers.getStage();
+        if (stage != null) {
+            Viewport viewport = stage.getViewport();
+            if (viewport != null) {
+                Camera camera = viewport.getCamera();
+                if (camera != null) {
+                    if (camera.frustum != null
+                        && (!camera.frustum.boundsInFrustum(xo, yOff - font.actualCellHeight - 1f, 0f, font.actualCellWidth, font.actualCellHeight, 0f)
+                        || !camera.frustum.boundsInFrustum(xo + font.actualCellWidth * (layers.gridWidth - 1), yo, 0f, font.actualCellWidth, font.actualCellHeight, 0f))) {
+                        frustum = camera.frustum;
+                    }
+                }
+            }
+        }
+        Epigon.filter.yMul = 0.9f * conditionY;
+        Epigon.filter.cwMul = 0.95f * conditionCw;
+        Epigon.filter.cmMul = 0.95f * conditionCm;
+        Epigon.filter.yAdd = 0.9f * conditionYAdd;
+        Epigon.filter.cwAdd = 0.95f * conditionCwAdd;
+        Epigon.filter.cmAdd = 0.95f * conditionCmAdd;
+
+        font.draw(batch, walls, xo - font.actualCellWidth * 0.25f, yo, 3, 3);
+//                font.draw(batch, walls, xo - font.actualCellWidth * 0.25f, yo, 3, 3);
+
+        font.configureShader(batch);
+        if (frustum == null) {
+            for (int i = 0; i < len; i++) {
+                layers.layers.get(i).draw(batch, font, xo, yOff);
+            }
+
+        } else {
+            for (int i = 0; i < len; i++) {
+                layers.layers.get(i).draw(batch, font, frustum, xo, yOff);
+            }
+        }
+        Epigon.filter.yMul = 1.05f * conditionY;
+        Epigon.filter.cwMul = 1.4f * conditionCw;
+        Epigon.filter.cmMul = 1.4f * conditionCm;
+        Epigon.filter.yAdd = 1.05f * conditionYAdd;
+        Epigon.filter.cwAdd = 1.4f * conditionCwAdd;
+        Epigon.filter.cmAdd = 1.4f * conditionCmAdd;
+        int x, y;
+        for (int i = 0; i < layers.glyphs.size(); i++) {
+            TextCellFactory.Glyph glyph = layers.glyphs.get(i);
+            if (glyph == null) {
+                continue;
+            }
+            glyph.act(Gdx.graphics.getDeltaTime());
+            if (!glyph.isVisible()
+                || (x = Math.round((gxo = glyph.getX() - xo) / font.actualCellWidth)) < 0 || x >= layers.gridWidth
+                || (y = Math.round((gyo = glyph.getY() - yo) / -font.actualCellHeight + layers.gridHeight)) < 0 || y >= layers.gridHeight
+                || layers.backgrounds[x][y] == 0f || (frustum != null && !frustum.boundsInFrustum(gxo, gyo, 0f, font.actualCellWidth, font.actualCellHeight, 0f))) {
+                continue;
+            }
+            glyph.draw(batch, 1f);
+        }
+    }
 
     @Override
     public void resize(int width, int height) {
-        System.out.println("Resizing to (" + width + ", " + height);
         // if not going to fullscreen, probably
         if (Gdx.graphics.getDisplayMode().width != width && !config.displayConfig.maximized) {
             config.displayConfig.windowWidth = width;
